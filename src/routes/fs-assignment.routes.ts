ee4b959cefcfd6ad451a82d1234f23d8c6c10b14
ShardulS <shardul.conclusion1@gmail.com>
2023-07-10 11:15:37 +0530

    File System Assignment: 7/7/23

--- src/routes/fs-assignment.routes.ts
+++ src/routes/fs-assignment.routes.ts
@@ -2,8 +2,8 @@ import multer from "multer";
 import express from "express";
 import { fscontroller } from "../controllers/fs.controller";
 
-const fs = require("fs");
 
+//This is the multer configuration to upload a file
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, `src/public/uploads/text`); // Specify the directory where you want to store uploaded files
@@ -16,5 +16,8 @@ const upload = multer({ storage: storage });
 
 export const fsRouter = express.Router();
 
+//Endpoint to upload a file
 fsRouter.post("/upload", upload.single("file"), fscontroller.uploadFile);
+
+//Endpoint to create a new file
 fsRouter.post("/merge-files", fscontroller.mergeAndCreateFile);
