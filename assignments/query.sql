315d32b5ac55cdcee6f7abfbac9d74b47ac83b12
ShardulS <shardul.conclusion1@gmail.com>
2023-07-12 12:10:17 +0530

    add: Queries related to the Follower Management

--- assignments/query.sql
+++ assignments/query.sql
@@ -1,13 +1,53 @@
+-- Create follower management and user profile - 
+-- Schema Link - 
+-- click on - https://drive.google.com/file/d/1VwZFPc6gNk5ddMDLV-fcAHj5uX-9Zd9u/view?ts=64ac4ca9
 
-Schema Link - 
-click on - https://drive.google.com/file/d/1VwZFPc6gNk5ddMDLV-fcAHj5uX-9Zd9u/view?ts=64ac4ca9
+-- Queries considering 
+-- User with userId - '39814192-9958-4304-b31f-c6d4d7a92446' is logged in.
+-- Another user with userId - '6a4d246c-a220-42a5-b338-ca62a079540f' can be a followeror following.
 
+-- 1. User should be able to follow other user. --
 
+INSERT INTO followers (userId, following, followingId) VALUES ('39814192-9958-4304-b31f-c6d4d7a92446', true, '6a4d246c-a220-42a5-b338-ca62a079540f');
 
+-- If record is already present
+UPDATE followers SET following = true WHERE userId = '39814192-9958-4304-b31f-c6d4d7a92446' AND followerId = '6a4d246c-a220-42a5-b338-ca62a079540f';
 
+-- 2. User should be able to unfollow other user.
 
+-- Only Update Value if already exists in table
+UPDATE followers SET following = false WHERE userId = '39814192-9958-4304-b31f-c6d4d7a92446' AND followerId = '6a4d246c-a220-42a5-b338-ca62a079540f';
 
+-- 3. User should be able to see total followers and following count. 
 
+-- To Count Following
+SELECT COUNT(userId) FROM followers WHERE following=true AND userId = '39814192-9958-4304-b31f-c6d4d7a92446';
+
+-- To Count Followers - 
+SELECT COUNT(userId) FROM followers WHERE followers=true AND userId = '39814192-9958-4304-b31f-c6d4d7a92446';
+
+-- 4. User should be able to see followers list.
+
+SELECT u.* FROM users u JOIN followers f ON u.userId = f.followerid WHERE f.followers = true AND u.userId = '39814192-9958-4304-b31f-c6d4d7a92446';
+
+-- 5. User should be able to see following list.
+
+SELECT u.*, f.followerid FROM users u JOIN followers f ON u.userId = f.followerid WHERE f.followers = true AND u.userId = '39814192-9958-4304-b31f-c6d4d7a92446';
+
+-- 6. User should be able to remove follower or following. [single follower at once]
+
+DELETE FROM followers WHERE followerid = '6a4d246c-a220-42a5-b338-ca62a079540f' AND userId = '39814192-9958-4304-b31f-c6d4d7a92446'
+
+-- 7. User should be able to see profile of another user.
+
+-- User profile should consist basic details 
+--     a. name
+--     b. image
+--     c. age
+--     d. username
+--     e. emailId"
+
+SELECT u.* FROM users u WHERE u.userId = '6a4d246c-a220-42a5-b338-ca62a079540f';
 
 
 
